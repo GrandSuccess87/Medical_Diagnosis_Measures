c262fb7f0e9efe83ab5ec7b0eff279f3bddf7ce7
Quintessa <anderson.quintessa@gmail.com>
2018-12-15 18:05:44 -0600

    setting up code for reading and writing to remote file

--- app_test/server.js
+++ app_test/server.js
@@ -1,3 +1,4 @@
+//Writing to the remote file://
 var WebHDFS = require('webhdfs');
 var hdfs = WebHDFS.createClient();
 
@@ -16,4 +17,27 @@ remoteFileStream.on('error', function onError (err) {
 remoteFileStream.on('finish', function onFinish () {
   // Upload is done
   console.log('Sample Data Uploaded Successfully');
+});
+
+//Reading from the remote file://
+var WebHDFS = require('webhdfs');
+var hdfs = WebHDFS.createClient({
+  user: 'webuser',
+  host: 'localhost',
+  port: 80,
+  path: '/webhdfs/v1'
+});
+
+var remoteFileStream = hdfs.createReadStream('/path/to/remote/file');
+
+remoteFileStream.on('error', function onError (err) {
+  // Do something with the error
+});
+
+remoteFileStream.on('data', function onChunk (chunk) {
+  // Do something with the data chunk
+});
+
+remoteFileStream.on('finish', function onFinish () {
+  // Upload is done
 });
