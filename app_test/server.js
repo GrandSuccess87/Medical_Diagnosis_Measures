07d94357f2da5e87ae7be5bec2204fe28ce5790d
Quintessa <anderson.quintessa@gmail.com>
2018-12-15 18:01:11 -0600

    updated package.json, setting data upload function in server.js, renamed sample data filename

--- app_test/server.js
+++ app_test/server.js
@@ -1,15 +1,19 @@
 var WebHDFS = require('webhdfs');
 var hdfs = WebHDFS.createClient();
 
-var localFileStream = fs.createReadStream('');
+var localFileStream = fs.createReadStream('./data/Sample_Data');
 var remoteFileStream = hdfs.createWriteStream('/path/to/remote/file');
 
 localFileStream.pipe(remoteFileStream);
 
 remoteFileStream.on('error', function onError (err) {
   // Do something with the error
+  if(err) {
+    throw err
+  };
 });
 
 remoteFileStream.on('finish', function onFinish () {
   // Upload is done
+  console.log('Sample Data Uploaded Successfully');
 });
