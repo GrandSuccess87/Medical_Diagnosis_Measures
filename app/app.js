f2a5e74686737f2b5ba8140deca231c775b20866
Quintessa <anderson.quintessa@gmail.com>
2018-12-17 13:31:41 -0600

    updated code in app.js - to store data locally

--- app/app.js
+++ app/app.js
@@ -1,5 +1,7 @@
 const fs = require('file-system');
 const csv = require('fast-csv');
+var WebHDFS = require('webhdfs');
+var hdfs = WebHDFS.createClient();
 
 let localFileStream = fs.createReadStream('./data/Sample_Data_2016.csv');
 // console.log("Hit Local File Stream");
@@ -14,7 +16,29 @@ localFileStream.pipe(csv())
         // console.log(data);
     }
   })
+  .on('error', function onError (err) {
+    // Do something with the error
+    // if(err) throw err;
+    console.log("Error: " + err);
+  })
   .on('end', function(data){
     // if(err) console.log("Error: " + err);
     console.log('Read Finished');
+  });
+
+
+  // var localFileStream = fs.createReadStream('');
+  var remoteFileStream = hdfs.createWriteStream('examples/src/main/resources/Sample_Data_2016.csv');
+
+  localFileStream.pipe(remoteFileStream);
+
+  remoteFileStream.on('error', function onError (err) {
+    // Do something with the error
+    // if(err) throw err;
+    console.log("Error: " + err);
+  });
+
+  remoteFileStream.on('finish', function onFinish () {
+    // Upload is done
+    console.log('Upload Finished');
   });
