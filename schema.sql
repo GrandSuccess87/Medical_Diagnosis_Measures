1eca6be292942c11230755962d5c6f7e5ad44c53
Quintessa <anderson.quintessa@gmail.com>
2018-12-17 01:33:41 -0600

    updated schema.sql file, deleted the first path

---
+++ schema.sql
@@ -0,0 +1,7 @@
+CREATE TEMPORARY VIEW MedicalMeasuresTable
+USING org.apache.spark.sql.json
+OPTIONS (
+  path spark.read.format("csv").option("header", "true").load("examples/src/main/resources/Sample_Data_2016.csv")
+)
+
+SELECT * FROM MedicalMeasuresTable
