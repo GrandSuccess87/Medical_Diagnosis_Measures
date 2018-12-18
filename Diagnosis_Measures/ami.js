edca304368fdf99bbfb34325b8c56e8d9e3751b0
Quintessa <anderson.quintessa@gmail.com>
2018-12-18 14:25:46 -0600

    updated code for query statements

--- Diagnosis_Measures/ami.js
+++ Diagnosis_Measures/ami.js
@@ -3,24 +3,51 @@ const ami = process.argv[2];
 const csvFilePath=('./data/SampleData2016.csv');
 const csv=require('csvtojson');
 
+var count = 0;
+
 csv()
 .fromFile(csvFilePath)
 .then((jsonObj)=>{
-  // console.log(jsonObj);
-    // var patientData = jsonObj;
-    patientData = JSON.stringify(jsonObj);
-    console.log(patientData);
-    // console.log(patientData.encounter_id);
+
+  if(count > 0) {
+
+    // console.log(jsonObj);
+      // var patientData = jsonObj;
+      patientData = JSON.stringify(jsonObj);
+      console.log(patientData);
+
+      patientData.forEach(function(value, index) {
 
 
-    // Get specific patient info for current index
-        var race = patientData.race;
-        console.log(race);
-        // Get specific article info for current index
-    // var article = NYTData.response.docs[i];
+        // print the insert statemtnts to the console
+        var query = "INSERT INTO MedicalMeasuresDF " +
+        "(encounter_id, patient_nbr) " +
+        "VALUES ('value.encounter_id', 'value.patient_nbr')";
+        console.log(query);
 
 
-})
+
+              // Get specific patient info for current index
+                  // var race = value.race;
+                  // console.log("race: " + race);
+                  //
+                  // // console.log(jsonObj);
+                  //   // var patientData = jsonObj;
+                  //   patientData = JSON.stringify(jsonObj);
+                  //   console.log(patientData);
+                  //   // console.log(patientData.encounter_id);
+                  //
+                  //
+                  //   // Get specific patient info for current index
+                  //       var race = patientData.race;
+                  //       console.log("race: " + race);
+
+
+                        return;
+      });
+    }
+      count ++;
+  });
 
 // Async / await usage
 const jsonArray = csv().fromFile(csvFilePath);
